7879632d4ee977a5fade8dd22414daa2d5932e12
Saturday <2908532087@qq.com>
2026-09-18 11:25:19 +0800

    这个是process全局对象的几个用法，用来管理这个文件的进程

--- http_projects/process.js
+++ http_projects/process.js
@@ -1,4 +1,8 @@
-console.log(process.getuid())
-console.log(process.getgid())
-console.log(process.pid)
-console.log(process.argv)
+http.createServer(callback).listen(80, function () {
+    var env = process.env,
+        uid = parseInt(env['SUDO_UID'] || process.getuid(), 10),
+        gid = parseInt(env['SUDO_GID'] || process.getgid(), 10);
+
+    process.setgid(gid);
+    process.setuid(uid);
+});
